aefcf6d4dd8c468f35abb89092f35628e80d9633
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-03-27 19:57:45 +0530

    Add files via upload

---
+++ main (8).cpp
@@ -0,0 +1,16 @@
+// print pattern Inverted..each element equal to number of rows
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cout<<"enter number:";
+  cin>>n;
+  for(int i=1;i<=n;i++){
+    for(int j=1;j<=n-i+1;++j){
+      cout<< i <<" ";
+    }
+    cout<<endl;
+  }
+  return 0;
+}
